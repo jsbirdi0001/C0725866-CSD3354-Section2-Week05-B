95180862b2fde17c19f19017138b38d3c1bf7cf0
visualstudio <visualstudio@122-PC31>
2019-02-06 11:47:35 -0500

    Changes Made

--- C0725866/Program.cs
+++ C0725866/Program.cs
@@ -4,46 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0725866
+namespace C0731712
 {
     class Program
     {
         static void Main(string[] args)
         {
-            LinkedList a = new LinkedList();
+            linkedlist a = new linkedlist();
+            a.run();
+            a.walkoverthelist();
         }
-
     }
 
-    class Dog
+    class dog
     {
-        public static int NumberofDogs = 0;
-        public Dog()
+        public static int numberofdogs = 0;
+        public dog()
         {
-
+            dog.numberofdogs++;
         }
-        String DogName;
-        String DogBreed;
-         
+        public string dogname;
+        public string dogbreed;
+        public dog nextdog;
     }
-    class LinkedList
+
+    class linkedlist
     {
-        public Dog Head;
-        Dog Peanut = new Dog();
+        public dog current;
+        public dog head;
+        public dog peanut = new dog();
+        public dog fifi = new dog();
+        public dog jordan = new dog();
+        public dog fido = new dog();
+        public dog roy = new dog();
+        public dog coco = new dog();
+
         public void run()
         {
-            Head = Peanut;
-            Peanut.DogName = "Yoo";
-        }
-    }
+            head = peanut;
+            peanut.dogname = "peanut";
+            peanut.dogbreed = "bichon";
+            peanut.nextdog = fifi;
 
-    public void WalkOverTheList()
-    {
-        Current = Head;
-        while (Current != null)
+            fifi.dogname = "fifi";
+            fifi.dogbreed = "poodle";
+            fifi.nextdog = jordan;
+
+            jordan.dogname = "jordan";
+            jordan.dogbreed = "german sheppard";
+            jordan.nextdog = fido;
+
+            fido.dogname = "fido";
+            fido.dogbreed = "beagle";
+            fido.nextdog = roy;
+
+            roy.dogname = "Roy";
+            roy.dogbreed = "Spaniel";
+            roy.nextdog = coco;
+
+            coco.dogname = "Coco";
+            coco.dogbreed = "beagle";
+            coco.nextdog = null;
+        }
+
+        public void walkoverthelist()
         {
-            Console.WriteLine(Current.DogName);
+            current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.dogname);
+                current = current.nextdog;
+            }
         }
     }
 }
-
