fb8279f2984d3697685e52926841b3cabf7c972b
visualstudio <visualstudio@122-PC31>
2019-02-06 11:36:41 -0500

    11.30 Commit

--- C0725866/Program.cs
+++ C0725866/Program.cs
@@ -10,6 +10,40 @@ namespace C0725866
     {
         static void Main(string[] args)
         {
+            LinkedList a = new LinkedList();
+        }
+
+    }
+
+    class Dog
+    {
+        public static int NumberofDogs = 0;
+        public Dog()
+        {
+
+        }
+        String DogName;
+        String DogBreed;
+         
+    }
+    class LinkedList
+    {
+        public Dog Head;
+        Dog Peanut = new Dog();
+        public void run()
+        {
+            Head = Peanut;
+            Peanut.DogName = "Yoo";
+        }
+    }
+
+    public void WalkOverTheList()
+    {
+        Current = Head;
+        while (Current != null)
+        {
+            Console.WriteLine(Current.DogName);
         }
     }
 }
+
